2101fd7adc67806082fd2d1ae3b842be0d17d7f3
jayaimzzz <jameshorton@gmail.com>
2019-04-18 15:09:18 -0400

    created test_integration.py file

---
+++ test_integration.py
@@ -0,0 +1,11 @@
+# from flask_testing import TestCase
+# from app import app
+
+# class MyTest(TestCase)
+#     def create_app(self)
+#         app.
+
+import pytest
+from app import app
+
+def test_app_index()
