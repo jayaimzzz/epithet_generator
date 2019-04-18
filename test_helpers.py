046684927edecef8dfa6f257a0ee0b61db852b57
jayaimzzz <jameshorton@gmail.com>
2019-04-18 12:47:47 -0400

    added unittest for EpithetGenerator

--- test_helpers.py
+++ test_helpers.py
@@ -1,10 +1,12 @@
-from helpers import Vocabulary
+from helpers import Vocabulary, EpithetGenerator
 import os
 import pytest
 
+path_to_data_json = os.path.join("resources","data.json")
+data = Vocabulary.read_json(path_to_data_json)
+e_gen = EpithetGenerator(path_to_data_json)
+
 def test_read_json_success():
-    path_to_data_json = os.path.join("resources","data.json")
-    data = Vocabulary.read_json(path_to_data_json)
     assert isinstance(data, dict)
     assert "Column 1" and "Column 2" and "Column 3" in data
     assert "Column 4" not in data
@@ -17,4 +19,22 @@ def test_read_json_failure():
     with pytest.raises(Exception):
         Vocabulary.read_json(path_to_data_csv)
 
+def test_get_random_words():
+    epithet = e_gen.get_random_words()
+    epithet_list = epithet.split(" ")
+    assert isinstance(epithet, str)
+    assert len(epithet_list) == 3
+    assert epithet_list[0] in data.get("Column 1")
+    assert epithet_list[1] in data.get("Column 2")
+    assert epithet_list[2] in data.get("Column 3")
+
+
+def test_get_epithets():
+    epithets = e_gen.get_epithets(5)
+    assert isinstance(epithets, list)
+    assert len(epithets) == 5
+    assert epithets[0].split(" ")[0] in data.get("Column 1")
+    assert epithets[4].split(" ")[2] in data.get("Column 3")
+    with pytest.raises(IndexError):
+        epithets[5]
     
